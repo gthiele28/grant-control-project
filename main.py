1f0953c56ec89bfffa333129967d904aeacf14d4
grant <gthiele2028@berkeley.edu>
2025-09-10 13:40:45 -0700

    literally the worst possible

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def controller(x):
         ndarray: numpy array of shape (2,) containing [fwd acceleration, steering rate]
     """
     ... # YOUR CODE HERE
+    return np.array([1,0])
 
 sim = Simulator(controller)
 sim.run()
